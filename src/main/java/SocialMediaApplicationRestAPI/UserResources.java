acc264afe60929bbb45b4662ab374b71335450a3
Raj Shah <shahraj0805@gmail.com>
2026-01-15 16:51:27 +0530

    Update UserResources.java

--- src/main/java/SocialMediaApplicationRestAPI/UserResources.java
+++ src/main/java/SocialMediaApplicationRestAPI/UserResources.java
@@ -1,23 +1,38 @@
 package SocialMediaApplicationRestAPI;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Collections;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 public class UserResources {
-    private UserDaoService service;
+
+    private final UserDaoService service;
+
     public UserResources(UserDaoService service) {
         this.service = service;
     }
 
-    @GetMapping("/users/{ID}")
-    public List<User> retriveUsers(@PathVariable int ID){
-        return Collections.singletonList(service.findOne(ID));
+    // GET all users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
 
-//        return service.findAll();
+    // GET single user
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+        return user;
+    }
+
+    // POST new user
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return service.save(user);
     }
 }
+
